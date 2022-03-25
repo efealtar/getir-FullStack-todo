f8d7222d2a0a5d74ddfd70e86a33367184b7f908
EFE TILU <41242722+efetilu@users.noreply.github.com>
2022-03-26 02:40:58 +0300

    dispatch deendency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getTodosAsync());
-	}, []);
+	}, [dispatch]);
 
 	const { status } = useSelector((state: any) => state);
 
